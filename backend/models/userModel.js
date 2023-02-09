b8d0faac2de57d885dee41a8060d1444f8c158b5
satyaprakash singh <satyamprakash448Qgmail.com>
2023-02-09 18:42:30 +0530

    userModel

---
+++ backend/models/userModel.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "please add a name"],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please enter a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "please add a password"],
+      minLength: [6, "password must be up to 6 characters"],
+      maxLength: [23, "password must not be more than 23 characters"],
+    },
+    photo: {
+      type: String,
+      required: [true, "please add a photo"],
+      default:
+        "https://img.freepik.com/free-psd/3d-illustration-person_23-2149436192.jpg?size=338&ext=jpg&ga=GA1.1.2003853548.1675917338&semt=sph",
+    },
+    phone: {
+      type: String,
+      default: "+91",
+    },
+    bio: {
+      type: String,
+      maxLength: [250, "bio must not be more than 250 characters"],
+      default: "bio",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,26 @@ const cors = require("cors");
 
 const app = express();
 
+//Middleware
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+
 const PORT = process.env.PORT || 8080;
+
+//Routes
+app.get("/", (req,res)=>{
+    res.send("Home Page")
+})
 
 //Connect to MongoDB and start server
 mongoose
 .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
 app.listen(PORT, () => {
-console.log(`Server running on Port ${PORT}`);
+console.log(`Server running on Port ${PORT} http://localhost:8080/`);
 });
 })
 .catch((err) => {
